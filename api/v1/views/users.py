efdd3c6b450464a3ba8dab7cfa92f61bab217d14
Samuel Amihere <amisam2000@gmail.com>
2024-02-20 09:14:52 +0800

    check import order

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ objects that handle all default RestFul API actions for Users """
+from flask import abort, jsonify, make_response, request
+from api.v1.views import app_views
 from models.user import User
 from models import storage
-from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
 
